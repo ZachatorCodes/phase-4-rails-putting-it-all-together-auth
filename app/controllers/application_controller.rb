9bf11be8382b4c7a1b4a39d6b74bdb0dfc33c309
Zach Schwartz <zachatorcodes@gmail.com>
2023-09-19 14:50:39 -0600

    Finished main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  before_action :authorize
+
+  private
+
+  def authorize
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Not Found" }, status: :not_found
+  end
 
 end
